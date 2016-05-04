86754cc89f539a817517b9fdfaa38b198f692ca9
alexandrekong <alexandre.kong@gmail.com>
2016-05-04 08:07:49 +0200

    Clean

---
+++ wp-config.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * La configuration de base de votre installation WordPress.
+ *
+ * Ce fichier contient les réglages de configuration suivants : réglages MySQL,
+ * préfixe de table, clefs secrètes, langue utilisée, et ABSPATH.
+ * Vous pouvez en savoir plus à leur sujet en allant sur
+ * {@link http://codex.wordpress.org/fr:Modifier_wp-config.php Modifier
+ * wp-config.php}. C'est votre hébergeur qui doit vous donner vos
+ * codes MySQL.
+ *
+ * Ce fichier est utilisé par le script de création de wp-config.php pendant
+ * le processus d'installation. Vous n'avez pas à utiliser le site web, vous
+ * pouvez simplement renommer ce fichier en "wp-config.php" et remplir les
+ * valeurs.
+ *
+ * @package WordPress
+ */
+
+// ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
+/** Nom de la base de données de WordPress. */
+define('DB_NAME', 'portofoliov2');
+
+/** Utilisateur de la base de données MySQL. */
+define('DB_USER', 'root');
+
+/** Mot de passe de la base de données MySQL. */
+define('DB_PASSWORD', 'root');
+
+/** Adresse de l'hébergement MySQL. */
+define('DB_HOST', 'localhost');
+
+/** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** Type de collation de la base de données.
+  * N'y touchez que si vous savez ce que vous faites.
+  */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Clefs uniques d'authentification et salage.
+ *
+ * Remplacez les valeurs par défaut par des phrases uniques !
+ * Vous pouvez générer des phrases aléatoires en utilisant
+ * {@link https://api.wordpress.org/secret-key/1.1/salt/ le service de clefs secrètes de WordPress.org}.
+ * Vous pouvez modifier ces phrases à n'importe quel moment, afin d'invalider tous les cookies existants.
+ * Cela forcera également tous les utilisateurs à se reconnecter.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'p{:yA27mX|BQ_87>{4fCGxgm+]Ode`I2{M%|aEC9DQ/KB7!)>i-@/R~w~%6+H1_%');
+define('SECURE_AUTH_KEY',  '{P<S;No4,yS)dl(99~=j?;m@/Wsl f(3D>M8-vFf.H02<+uQwT0fR2-J `le@w^b');
+define('LOGGED_IN_KEY',    ',QIE]TyF<k-iZmqn|w|ziz=(~4^S-fuD{mK/4/mcg5& AS(V`Ni9wlFJ_l$j-_BN');
+define('NONCE_KEY',        '?J$#Id+nzB|u9<o.?es!AW#v +@[]~=8qH|yur6/9W%>+d{:?#?{N`W.#xuEr`bA');
+define('AUTH_SALT',        '%sptqU1Uw3vf44k3-7NQRKKN6k0eqfKi6ob~,57-SK_IeY,-{;!>}/y,n?/rcp7v');
+define('SECURE_AUTH_SALT', 'zx[#@E+/wa{kSi0hGLn>-0@=L,cjN8CoeCb|%p2o/<,|B2J.dGY]I|EVwwGO7<J)');
+define('LOGGED_IN_SALT',   'v`eu>qACvHessLZV%ZG!_TeWNu)QLHv}d]Zq;~0bl8.HFy$Hz_fSyQg`f=C~/mR~');
+define('NONCE_SALT',       'v0JGdA0{1TGzx}irz+_4O+AgC8P4r<[rdq@+]fD#[,@O{$]17Dp*e8x|+.-)>|*e');
+/**#@-*/
+
+/**
+ * Préfixe de base de données pour les tables de WordPress.
+ *
+ * Vous pouvez installer plusieurs WordPress sur une seule base de données
+ * si vous leur donnez chacune un préfixe unique.
+ * N'utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * Pour les développeurs : le mode déboguage de WordPress.
+ *
+ * En passant la valeur suivante à "true", vous activez l'affichage des
+ * notifications d'erreurs pendant vos essais.
+ * Il est fortemment recommandé que les développeurs d'extensions et
+ * de thèmes se servent de WP_DEBUG dans leur environnement de
+ * développement.
+ */
+define('WP_DEBUG', false);
+
+/* C'est tout, ne touchez pas à ce qui suit ! Bon blogging ! */
+
+/** Chemin absolu vers le dossier de WordPress. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Réglage des variables de WordPress et de ses fichiers inclus. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/themes/portofolio/home.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+get_header(); ?>
+<div class="wrapper">
+
+  <div class="banniere">
+  	<div class="slider-texte">
+  		<div class="owl-carousel" id="owl-carousel">
+  			<div class="item">Passionnant</div>
+  			<div class="item">Distingué</div>
+  			<div class="item">Inspirant</div>
+  		</div>
+  		<div class="slogan">
+  			Webdesigner / developpeur / intégrateur
+  		</div>
+  		<div class="socialmedia">
+  			<ul>
+  				<li><a href="#"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i></a></li>
+  				<li><a href="#"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a></li>
+  				<li><a href="#"><i class="fa fa-instagram fa-lg" aria-hidden="true"></i></a></li>
+  			</ul>
+  		</div>
+  	</div>
+  </div>
+
+</div>
+
+<div class="wrapper-in">
+
+  <div class="lastcrea">
+    <h3>Dernières créations</h3>
+    <hr>
+    <p>Quel meilleur moyen de me présenter ?<br>Voici mes dernières réalisations afin de vous faire une idée de ce que je peux apporter à votre projet personnel.</p>
+    <div class="creas">
+      <div class="row">
+        <div class="small-4 columns">
+          <div class="thumb"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/portofolio1.jpg" alt=""></div>
+        </div>
+        <div class="small-4 columns">
+          <div class="thumb"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/portofolio2.jpg" alt=""></div>
+        </div>
+        <div class="small-4 columns">
+          <div class="thumb"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<div class="greyband">
+  <div class="texte">
+    <h1>Hello</h1>
+    <hr>
+    <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. </p>
+  </div>
+</div>
+
+<div class="greyband-bottom"></div>
+<div class="round-competences">
+  <div class="in">
+    <div class="rond"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/icons/web.png" alt=""></div>
+    <div class="rond"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/icons/paint.png" alt=""></div>
+    <div class="rond"><img src="<?php echo get_stylesheet_directory_uri() ?>/images/icons/people.png" alt=""></div>
+  </div>
+</div>
+<div class="texte-competences">
+  <div class="in">
+    <div>CMS / CSS</div>
+    <div>Webdesign</div>
+    <div>Référencement</div>
+  </div>
+</div>
+
+<div class="wrapper-in">
+  <div class="skills">
+    <div class="row">
+      <div class="medium-4 columns">
+        <div class="inset">
+          <div class="in">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/icons/eye.png" alt="">
+            <h5>Competences</h5>
+            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux</p>
+          </div>
+        </div>
+      </div>
+      <div class="medium-4 columns">
+        <div class="inset">
+          <div class="in">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/icons/eye.png" alt="">
+            <h5>Competences</h5>
+            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux</p>
+          </div>
+        </div>
+      </div>
+      <div class="medium-4 columns">
+        <div class="inset">
+          <div class="in">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/icons/eye.png" alt="">
+            <h5>Competences</h5>
+            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="medium-4 columns">
+        <div class="inset">
+          <div class="in">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/icons/eye.png" alt="">
+            <h5>Competences</h5>
+            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux</p>
+          </div>
+        </div>
+      </div>
+      <div class="medium-4 columns">
+        <div class="inset">
+          <div class="in">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/icons/eye.png" alt="">
+            <h5>Competences</h5>
+            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux</p>
+          </div>
+        </div>
+      </div>
+      <div class="medium-4 columns">
+        <div class="inset">
+          <div class="in">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/images/icons/eye.png" alt="">
+            <h5>Competences</h5>
+            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!--
+<div id="page" role="main">
+	<article class="main-content">
+	<?php if ( have_posts() ) : ?>
+
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
+		<?php endwhile; ?>
+
+		<?php else : ?>
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; // End have_posts() check. ?>
+
+		<?php /* Display navigation to next/previous pages when applicable */ ?>
+		<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
+			<nav id="post-nav">
+				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
+				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
+			</nav>
+		<?php } ?>
+
+	</article>
+	<?php get_sidebar(); ?>
+
+</div>-->
+
+<?php get_footer();
